be2c169e9d71b26d0811574d384d22e9c1679336
gmso <german.mene@gmail.com>
2021-06-03 21:54:24 +0200

    Add functionality and tests to ArgParser

--- recipys/ArgParser.py
+++ recipys/ArgParser.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple, AbstractSet
+import re
+from typing import List, Tuple, AbstractSet, Optional
 
 
 class ArgParser():
@@ -18,21 +19,21 @@ class ArgParser():
             "breakfast", "lunch", "dinner", "dessert"}
         self.args: List[str] = args
 
-    def parse(self) -> Tuple[str, List[str]]:
+    def parse(self) -> Tuple[Optional[str], Optional[List[str]]]:
         """
         Parses command line arguments
 
         Returns:
             Tuple with:
                 - str: meal name (default: None)
-                - List[str]: list of ingredients (default: None)
+                - List[str]: list of ingredients (default: List[None])
         """
 
         self.meal: str = self._get_meal()
         self.ingredients: List[str] = self._get_ingredients()
         return (self.meal, self.ingredients)
 
-    def _get_meal(self) -> str:
+    def _get_meal(self) -> Optional[str]:
         """
         Gets meal type from command line argument
 
@@ -45,11 +46,29 @@ class ArgParser():
                     return accepted_meal
         return None
 
-    def _get_ingredients(self) -> List[str]:
+    def _get_ingredients(self) -> Optional[List[str]]:
         """
         Gets meal type from command line argument
 
         Returns:
-            - str: meal name (default: None)
+            - List[str]: list of valid ingredients (default: None)
         """
-        pass
+        if "with" not in self.args:
+            return None
+        
+        start: int = 2 if self.meal else 1
+
+        if "with" is not self.args[start]:
+            return None
+
+        ingredients: List[str] = []
+        for _, arg in enumerate(self.args[start+1::]):
+            m = re.findall(r"\w",arg)
+            word = "".join(m)
+            if word:
+                ingredients.append(word)
+
+        if not ingredients:
+            return None
+            
+        return ingredients
--- tests/test_ArgParser.py
+++ tests/test_ArgParser.py
@@ -1,49 +1,76 @@
+import re
 from recipys.ArgParser import ArgParser
 from utility_randoms import random_string
 
 
 def test_ArgParser_construction():
-    cla = ["recipys"]
-    parser = ArgParser(cla)
+    parser = ArgParser(["recipys"])
     assert parser.accepted_meals == {
             "breakfast", "lunch", "dinner", "dessert"}
     assert parser.args
 
 
 def test_no_arguments():
-    cla = ["recipys"]
-    parser = ArgParser(cla)
+    parser = ArgParser(["recipys"])
     assert parser.parse() == (None, None)
 
 
 def test_meals_valid():
-    cla = ["recipys", "breakfast"]
-    parser = ArgParser(cla)
+    parser = ArgParser(["recipys", "breakfast"])
     assert parser.parse() == ("breakfast", None)
 
-    cla = ["recipys", "lunch"]
-    parser = ArgParser(cla)
+    parser = ArgParser(["recipys", "lunch"])
     assert parser.parse() == ("lunch", None)
 
-    cla = ["recipys", "dinner"]
-    parser = ArgParser(cla)
+    parser = ArgParser(["recipys", "dinner"])
     assert parser.parse() == ("dinner", None)
 
-    cla = ["recipys", "dessert"]
-    parser = ArgParser(cla)
+    parser = ArgParser(["recipys", "dessert"])
     assert parser.parse() == ("dessert", None)
 
 
 def test_meals_invalid():
-    cla = ["recipys", "desssert"]
-    parser = ArgParser(cla)
+    parser = ArgParser(["recipys", "desssert"])
     assert parser.parse() == (None, None)
 
-    cla = ["recipys", ","]
-    parser = ArgParser(cla)
+    parser = ArgParser(["recipys", "a", "lunch"])
     assert parser.parse() == (None, None)
 
     for w in range(10):
-        cla = ["recipys", random_string()]
-        parser = ArgParser(cla)
+        parser = ArgParser(["recipys", random_string()])
         assert parser.parse() == (None, None)
+
+
+def test_ingredients_valid():
+    parser = ArgParser(["recipys", "with", "banana"])
+    assert parser.parse() == (None, ["banana"])
+
+    parser = ArgParser(["recipys", "with", "garlic", "onion"])
+    assert parser.parse() == (None, ["garlic", "onion"])
+    
+    parser = ArgParser(["recipys", "with", "'*'#potato+´?."])
+    assert parser.parse() == (None, ["potato"])
+
+    for w in range(10):
+        rand_string = random_string()
+        parser = ArgParser(["recipys", "with", rand_string])
+        word  = "".join(re.findall(r"\w",rand_string))
+        if word:
+            assert parser.parse() == (None, [word])
+        else:
+            assert parser.parse() == (None, None)
+
+
+def test_ingredients_invalid():
+    parser = ArgParser(["recipys", "banana"]) # "with" missing
+    assert parser.parse() == (None, None)
+    
+    parser = ArgParser(["recipys", "chocolate", "with"]) # "with" misplaced
+    assert parser.parse() == (None, None)
+    
+    parser = ArgParser(["recipys", "with", "..."]) # invalid ingredient
+    assert parser.parse() == (None, None)
+
+    # one valid ingredient, invalid ingredients ignored
+    parser = ArgParser(["recipys", "with", "...", "???", "apple"])
+    assert parser.parse() == (None, ["apple"])
